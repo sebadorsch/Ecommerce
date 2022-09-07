0e8dffbfaac7a1b280460e225a4426ceed3a67d2
sebastiandorsch <sebastian@letsmade.co>
2022-09-07 00:17:08 -0300

    changes

--- src/Pages/Activation/Activation.js
+++ src/Pages/Activation/Activation.js
@@ -1,6 +1,14 @@
-import {Layout} from "../../Components/core/Layout";
+import { Layout } from "../../Components/core/Layout";
+import { useParams } from "react-router-dom";
 
 const Activate = () => {
+  const params = useParams()
+
+  const uid = params.uid
+  const token = params.token
+
+  console.log(uid)
+  console.log(token)
   return(
     <Layout>Activate succeeded!</Layout>
   )
