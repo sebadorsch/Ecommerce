7c4da763de7a5900364485481124b3bbe6b6dc36
sebastiandorsch <sebastian@letsmade.co>
2022-09-27 13:53:15 -0300

    href

--- src/Components/core/Navbar/Navbar.js
+++ src/Components/core/Navbar/Navbar.js
@@ -80,6 +80,7 @@ function Navbar({
           <Menu.Item>
             {({ active }) => (
               <a
+                href="#top"
                 onClick={logOutHandler}
                 className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
                 style={{cursor: 'pointer'}}
